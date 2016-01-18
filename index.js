d10b8d13b6d4ade111aaaa5300ff7a5839d81842
Aaron Vontell <vontell@mit.edu>
2016-01-18 16:25:10 -0500

    Testing github integration

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cool = require('cool-ascii-faces');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,8 +14,10 @@ app.get('/', function(request, response) {
   response.render('public/index.html');
 });
 
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
